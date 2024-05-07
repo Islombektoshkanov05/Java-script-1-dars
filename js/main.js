5e0632140f3a15e45361781266fad3db56786f36
Islombektoshkanov05 <169050123+Islombektoshkanov05@users.noreply.github.com>
2024-05-07 11:57:08 +0500

    java script 1-dars
    zeromax

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*console.log("Men faqat konsol oynasida ko'rinaman!");*/
+/*alert("Saytimizga xush kelibsiz!");*/
+/*document.write(200+200*5-100);*/
+
+/*let x = 5;*/
+/*let z = 7;*/
+/*x = 10;*/
+/*let salom = "Assalomu alaykum!";*/
+/*let x = 10, z = 7, y = 25;*/
+/*console.log(x+z-y);*/
+
+/* Data types in JS */
+/*let soz = "JavaScript"; */        /* string */
+/*let son = 5;*/                    /* number */
+/*document.write(typeof son);*/
+
+/* Math in JS */
+
+let x = 5;
+/* x += 5; */   /* x = 5 + 5 */
+/* x -= 2; */   /* x = 5 - 2 */
+/* x *= 5; */   /* x = 5 * 5 */
+/* x /= 2; */   /* x = 5 / 2 */
+/* let qoldiq = x % 5; */
+/* let natija = x**5; */
+/* ++x; */
+/* --x; */
+/* x++; */
+/* x--; */
+
+document.write(x + "salom");
+
